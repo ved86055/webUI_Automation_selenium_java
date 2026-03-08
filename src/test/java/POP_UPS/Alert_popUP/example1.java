dd1197f89d703a321f3b350299338fd997dc8d7c
Ved86055 <vedvshinde11@gmail.com>
2026-03-08 23:09:33 +0530

    AlertPopup

---
+++ src/test/java/POP_UPS/Alert_popUP/example1.java
@@ -0,0 +1,34 @@
+package POP_UPS.Alert_popUP;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class example1
+{
+    @Test
+    void handleAlertpopUp()
+    {
+        String name="vedprakash";
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        driver.findElement(By.id("name")).sendKeys(name);
+
+        driver.findElement(By.id("alertbtn")).click();
+
+        driver.switchTo().alert().getText();
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.id("confirmbtn")).click();
+
+        driver.switchTo().alert().getText();
+
+        driver.switchTo().alert().dismiss();
+    }
+}
